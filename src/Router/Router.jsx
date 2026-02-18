6691a66095bd03b13ec110303253b90f9f03e0ea
mdazim12 <azimuddin807@gmail.com>
2026-02-19 04:22:13 +0600

    auth layout added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,19 +1,32 @@
 import { createBrowserRouter } from "react-router";
 import RootLayouts from "../layouts/RootLayouts";
 import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
 
 
 
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component : RootLayouts,
-    children : [
-        {
-            index : true,
-            Component : Home
-        }
-    ]
+    {
+        path: "/",
+        Component: RootLayouts,
+        children: [
+            {
+                index: true,
+                Component: Home
+            }
+        ]
     },
+
+    {
+        path : "/",
+        Component : AuthLayout,
+        children : [
+            {
+                path : 'login',
+                element : <Login></Login>
+            }    
+        ]
+    }
 ]);
---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const AuthLayout = () => {
+    return (
+        <div className="hero bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    className="max-w-sm rounded-lg shadow-2xl"
+                />
+                <div>
+                    <Outlet></Outlet>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AuthLayout;
