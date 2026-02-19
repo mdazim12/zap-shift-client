1ef871ae832ab4a877c9b10a92816a1f8b64ea16
mdazim12 <azimuddin807@gmail.com>
2026-02-20 03:40:16 +0600

    login page form added

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import AuthImg from '../assets/authImage.png'
+import Logo from '../pages/shared/Logo/Logo';
+
 
 const AuthLayout = () => {
     return (
-        <div className="hero bg-base-200">
+        <div className="p-12 bg-base-200">
+            <div>
+                <Logo></Logo>
+            </div>
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
-                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                    className="max-w-sm rounded-lg shadow-2xl"
-                />
-                <div>
+                <div className='flex-1'>
+                    <img
+                        src={AuthImg}
+                        className="max-w-sm rounded-lg shadow-2xl"
+                    />
+                </div>
+                <div className='flex-1'>
                     <Outlet></Outlet>
                 </div>
             </div>
--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
     return (
         <div>
-            <h3>This is login</h3>
+            <form onSubmit={handleSubmit(onSubmit)} >
+                <fieldset className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email')} className="input" placeholder="Email" />
+
+                    <label className="label">Password</label>
+                    <input type="password"
+                        {...register('password', {
+                            required: true,
+                            minLength: 6
+                        })}
+                        className="input" placeholder="Password" />
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+            </form>
         </div>
     );
 };
 
-export default Login;
+export default Login; 
